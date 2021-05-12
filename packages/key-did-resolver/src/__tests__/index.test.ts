50dda1b0973ef29b8c8534619d7c71ddebf725e2
Brent Shambaugh <brent.shambaugh@gmail.com>
2021-05-12 17:57:05 -0500

    add secp256r1.ts tests in key-did-resolver

--- packages/key-did-resolver/src/__tests__/index.test.ts
+++ packages/key-did-resolver/src/__tests__/index.test.ts
@@ -51,7 +51,36 @@ describe('Index mapper', () => {
     doc = await resolve('did:key:zruuPojWkzGPb8sVc42f2YxcTXKUTpAUbdrzVovaTBmGGNyK6cGFaA4Kp7SSLKecrxYz8Sc9d77Rss7rayYt1oFCaNJ', parsedDid, {}, { accept: 'application/did+json' })
     expect(doc).toMatchSnapshot()
 
+    parsedDid = {
+      id: "zDnaeUKTWUXc1HDpGfKbEK31nKLN19yX5aunFd7VK1CUMeyJu",
+      did: 'did:key:zDnaeUKTWUXc1HDpGfKbEK31nKLN19yX5aunFd7VK1CUMeyJu',
+      method: "key",
+      didUrl: 'did:key:zDnaeUKTWUXc1HDpGfKbEK31nKLN19yX5aunFd7VK1CUMeyJu/some/path',
+      path: '/some/path'
+    }
+  
+    doc = await resolve('did:key:zDnaeUKTWUXc1HDpGfKbEK31nKLN19yX5aunFd7VK1CUMeyJu', parsedDid, {}, { accept: 'application/did+ld+json' })
+    expect(doc).toMatchSnapshot()
+  
+    doc = await resolve('did:key:zDnaeUKTWUXc1HDpGfKbEK31nKLN19yX5aunFd7VK1CUMeyJu', parsedDid, {}, { accept: 'application/did+json' })
+    expect(doc).toMatchSnapshot()
+
+    parsedDid = {
+      id: "z4oJ8emo5e6mGPCUS5wncFZXAyuVzGRyJZvoduwq7FrdZYPd1LZQbDKsp1YAMX8x14zBwy3yHMSpfecJCMDeRFUgFqYsY",
+      did: 'did:key:z4oJ8emo5e6mGPCUS5wncFZXAyuVzGRyJZvoduwq7FrdZYPd1LZQbDKsp1YAMX8x14zBwy3yHMSpfecJCMDeRFUgFqYsY',
+      method: "key",
+      didUrl: 'did:key:z4oJ8emo5e6mGPCUS5wncFZXAyuVzGRyJZvoduwq7FrdZYPd1LZQbDKsp1YAMX8x14zBwy3yHMSpfecJCMDeRFUgFqYsY/some/path',
+      path: '/some/path'
+    }
+  
+    doc = await resolve('did:key:z4oJ8emo5e6mGPCUS5wncFZXAyuVzGRyJZvoduwq7FrdZYPd1LZQbDKsp1YAMX8x14zBwy3yHMSpfecJCMDeRFUgFqYsY', parsedDid, {}, { accept: 'application/did+ld+json' })
+    expect(doc).toMatchSnapshot()
+  
+    doc = await resolve('did:key:z4oJ8emo5e6mGPCUS5wncFZXAyuVzGRyJZvoduwq7FrdZYPd1LZQbDKsp1YAMX8x14zBwy3yHMSpfecJCMDeRFUgFqYsY', parsedDid, {}, { accept: 'application/did+json' })
+    expect(doc).toMatchSnapshot()
+
   })
+
 
 })
 
--- packages/key-did-resolver/src/__tests__/secp256r1.test.ts
+++ packages/key-did-resolver/src/__tests__/secp256r1.test.ts
@@ -14,6 +14,30 @@ describe('Secp256r1 mapper', () => {
         expect(doc).toMatchSnapshot()
     })
 
+    it('successfully resolves the document from did', async () => {
+        const id = "zDnaeUKTWUXc1HDpGfKbEK31nKLN19yX5aunFd7VK1CUMeyJu"
+
+        const multicodecPubKey = multibase.decode(id)
+        varint.decode(multicodecPubKey) // decode is changing param multicodecPubKey as well
+        const pubKeyBytes = multicodecPubKey.slice(varint.decode.bytes)
+        const doc = await mapper.keyToDidDoc(pubKeyBytes, id)
+        expect(doc).toMatchSnapshot()
+    })
+
+    it('successfully resolves the document from did', async () => {
+        const id = "z4oJ8emo5e6mGPCUS5wncFZXAyuVzGRyJZvoduwq7FrdZYPd1LZQbDKsp1YAMX8x14zBwy3yHMSpfecJCMDeRFUgFqYsY"
+
+        const multicodecPubKey = multibase.decode(id)
+        varint.decode(multicodecPubKey) // decode is changing param multicodecPubKey as well
+        const pubKeyBytes = multicodecPubKey.slice(varint.decode.bytes)
+        const doc = await mapper.keyToDidDoc(pubKeyBytes, id)
+        expect(doc).toMatchSnapshot()
+    })
+
 })
 
+// compressed key
+// did:key:zDnaeUKTWUXc1HDpGfKbEK31nKLN19yX5aunFd7VK1CUMeyJu
 
+// uncompressed key
+// did:key:z4oJ8emo5e6mGPCUS5wncFZXAyuVzGRyJZvoduwq7FrdZYPd1LZQbDKsp1YAMX8x14zBwy3yHMSpfecJCMDeRFUgFqYsY
